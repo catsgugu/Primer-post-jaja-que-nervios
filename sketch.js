aa3f7ed8bb7fcee40005fd15ff5dfe059409a9b6
Giulia Pepe <gifipepevargas@gmail.com>
2025-08-11 16:58:04 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,4 +9,12 @@ function draw() {
 d = random(10, 60);
   fill(255,random(55,200));
   ellipse(mouseX,mouseY,d,d)
+
 }
+
+function keyPressed() {
+  if (key === 'p') {
+    save('dibujo.jpg');
+  }
+}
+ 
